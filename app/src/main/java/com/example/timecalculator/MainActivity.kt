da02dacfa497de09f894f677cbe6c9b66f71270f
Hellion <semakin.s.v@gmail.com>
2024-10-14 02:04:36 +0300

    added toolbar, menu and toasts

--- app/src/main/java/com/example/timecalculator/MainActivity.kt
+++ app/src/main/java/com/example/timecalculator/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.example.timecalculator
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var toolbarMain: Toolbar
 
     private lateinit var firstOperandET: EditText
     private lateinit var secondOperandET: EditText
@@ -19,9 +25,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var buttondDifBTN: Button
 
     private lateinit var resultTV: TextView
-
-    private lateinit var buttonResetBTN: Button
-    private lateinit var buttondExitBTN: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +36,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             insets
         }
 
+        toolbarMain = findViewById(R.id.toolbar_main)
+        setSupportActionBar(toolbarMain)
+        title = "Калькулятор времени"
+        toolbarMain.subtitle = "v. 1.0"
+        toolbarMain.setLogo(R.drawable.baseline_timelapse_24)
+
         firstOperandET = findViewById(R.id.firstOperandET)
         secondOperandET = findViewById(R.id.secondOperandET)
 
@@ -41,18 +50,41 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         resultTV = findViewById(R.id.resultTV)
 
-        buttonResetBTN = findViewById(R.id.buttonResetBTN)
-        buttondExitBTN = findViewById(R.id.buttonExitBTN)
-
         buttonSumBTN.setOnClickListener(this)
         buttondDifBTN.setOnClickListener(this)
-        buttonResetBTN.setOnClickListener(this)
-        buttondExitBTN.setOnClickListener(this)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.reset_menu_main -> {
+                firstOperandET.text.clear()
+                secondOperandET.text.clear()
+                resultTV.text = "Результат"
+                Toast.makeText(
+                    applicationContext,
+                    "Данные очищены",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+
+            R.id.exit_menu_main -> {
+                Toast.makeText(
+                    applicationContext,
+                    "Приложение закрыто",
+                    Toast.LENGTH_LONG
+                ).show()
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onClick(v: View){
-
-        var check = true
 
         if (firstOperandET.text.isEmpty() || secondOperandET.text.isEmpty()){
             return
@@ -66,16 +98,15 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             var result = when(v.id){
                 R.id.buttonSumBTN -> operation.operationResult(operation.sum())
                 R.id.buttonDifBTN -> operation.operationResult(operation.dif())
-                R.id.buttonResetBTN -> {
-                    firstOperandET.text.clear()
-                    secondOperandET.text.clear()
-                    check = false
-                }
 
-                R.id.buttonExitBTN -> finish()
                 else -> 0.0
             }
-            if (!check) resultTV.text = "Результат" else resultTV.text = result.toString()
+            resultTV.text = result.toString()
+            Toast.makeText(
+                applicationContext,
+                "Результат: $result",
+                Toast.LENGTH_LONG
+            ).show()
         } catch (e: DataFormatEditTextException){
             resultTV.text = e.message
         }
